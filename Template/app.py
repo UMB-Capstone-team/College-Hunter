e9e195dda01080c55d1db6f92b2ca118e6ed2aec
TheMiamest <joshuap314159@gmail.com>
2023-04-27 12:02:30 -0400

    added to college profile
    and added to search

--- Template/app.py
+++ Template/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session, jsonify, url_for
+from jinja2 import Template
 import requests
 import os
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
-# app.config['SECRET_KEY'] = ';lkjfdsa'
+app.config['SECRET_KEY'] = ';lkjfdsa'
 
 API_KEY = "MHgWdG6I183QDehzQpEp1ZYFmSjJqOytO8ICj7Xl"
 API_URL = "https://api.data.gov/ed/collegescorecard/v1/schools"
@@ -24,35 +25,41 @@ def search():
         "latest.admissions.admission_rate.overall",
         "latest.student.size",
         "school.city",
-        "school.state",
-        "school.school_url",
+        "school.state", 
+        "latest.admissions.sat_scores.average.overall",
         "id"
-        ]),
+    ]),
     }
 
     state = request.form.get("states")
-    min_tuition = int(request.form.get("min_tuition")) if request.form.get("min_tuition") else None
-    max_tuition = int(request.form.get("max_tuition")) if request.form.get("max_tuition") else None
-    min_admission_rate = float(request.form.get("min_admission_rate")) if request.form.get("min_admission_rate") else None
-    max_admission_rate = float(request.form.get("max_admission_rate")) if request.form.get("max_admission_rate") else None
-    min_size = int(request.form.get("min_size")) if request.form.get("min_size") else None
-    max_size = int(request.form.get("max_size")) if request.form.get("max_size") else None
+    min_tuition = int(request.form.get("min_tuition")) if request.form.get("min_tuition") is not None else None
+    max_tuition = int(request.form.get("max_tuition")) if request.form.get("max_tuition") is not None else None
+    min_admission_rate = float(request.form.get("min_admission_rate")) if request.form.get("min_admission_rate") is not None else None
+    max_admission_rate = float(request.form.get("max_admission_rate")) if request.form.get("max_admission_rate") is not None else None
+    min_size = int(request.form.get("min_size")) if request.form.get("min_size") is not None else None
+    max_size = int(request.form.get("max_size")) if request.form.get("max_size") is not None else None
+    min_sat = int(request.form.get("min_sat")) if request.form.get("min_sat") is not None else None
+    max_sat = int(request.form.get("max_sat")) if request.form.get("max_sat") is not None else None
 
     if state:
         params["school.state"] = state
     if min_tuition and max_tuition:
-        params["latest.cost.tuition.in_state__range"] = f"{min_tuition}..{max_tuition}"
+        params["latest.cost.tuition.out_of_state__range"] = f"{min_tuition}..{max_tuition}"
     if min_admission_rate and max_admission_rate:
         params["latest.admissions.admission_rate.overall__range"] = f"{min_admission_rate / 100}..{max_admission_rate / 100}"
     if min_size and max_size:
         params["latest.student.size__range"] = f"{min_size}..{max_size}"
-    
+    if min_sat and max_sat:
+        params["latest.admissions.sat_scores.average.overall__range"] = f"{min_sat}..{max_sat}"
+            
+    # params["_fields"] = ",".join(fields)
     response = requests.get(API_URL, params=params)
     response_data = response.json()
-
     colleges = response_data.get("results", [])
     print("API response status code:", response.status_code)
     print("API response data:", response.json())
+    # session['colleges'] = colleges
+    # return jsonify(colleges)
     return render_template("search_results.html", colleges=colleges)
 
 @app.route("/api_credits")
@@ -72,14 +79,29 @@ def college_profile(college_id):
             "latest.student.size",
             "school.city",
             "school.state",
-            "school.school_url",
+            "latest.admissions.sat_scores.average.overall",
+            "latest.aid.federal_loan_rate",
+            "latest.completion.completion_rate_4yr_150nt_pooled",
+            "latest.academics.program_percentage",
+            "latest.school.school_url",
             "id"
         ]),
     }
     response = requests.get(API_URL, params=params)
     college = response.json().get("results", [])[0]
-
+    print("API response status code:", response.status_code)
+    print("API response data:", response.json())
+    # return jsonify(college)
     return render_template("college_profile.html", college=college)
+
+def format_significant_majors(college):
+    majors = []
+    for key, value in college.items():
+        if key.startswith('latest.academics.program_percentage') and value >= 0.2:
+            formatted_key = key.replace('latest.academics.program_percentage.','').replace('_', ' ').title()
+            majors.append(formatted_key)
+    return ', '.join(majors)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
